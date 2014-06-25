4576560a0b38a27b6e26d438a44d9982e9c4ff89
jomc4437 <jordan.mcnally@colorado.edu>
2014-06-25 13:09:00 -0600

    Added area, and test cases

--- geometry_test.c
+++ geometry_test.c
@@ -144,7 +144,33 @@ START_TEST(test_2d_midpoint)
 
 }
 END_TEST
+/*coord_2d_area Test*/
+START_TEST(test_2d_area)
+{
+    coord_2d_t a;
+    coord_2d_t b;
+    coord_2d_t c;
+    double sol;
+    double act;
 
+    a.x = b.x = c.x = 0;
+    a.y = b.y = c.y = 0;
+    act = coord_2d_area(&a, &b, &c);
+    sol = 0;
+    ck_assert(act == sol);
+
+    a.x = 19;
+    a.y = 8;
+    b.x = 23;
+    b.y = 30;
+    c.x = 50;
+    c.y = 25;
+    act = coord_2d_area(&a, &b, &c);
+    sol = 307;
+    ck_assert(act == sol);
+
+}
+END_TEST
 /* coord_2d Test Suite */
 Suite* coord_2d_suite(void)
 {
@@ -162,15 +188,21 @@ Suite* coord_2d_suite(void)
     TCase* tc_2d_midpoint = tcase_create("coord_2d_midpoint");
     tcase_add_test(tc_2d_midpoint, test_2d_midpoint);
 
+    TCase* tc_2d_area = tcase_create("coord_2d_area");
+    tcase_add_test(tc_2d_area, test_2d_area);
+
     /* Add Cases to Suite */
     suite_add_tcase(s, tc_2d_eq);
     suite_add_tcase(s, tc_2d_dist);
     suite_add_tcase(s, tc_2d_midpoint);
+    suite_add_tcase(s, tc_2d_area);
 
     /* Return Suite */
     return s;
 
 }
+
+
 
 /* main: run test suites and set exit status */
 int main(void){
